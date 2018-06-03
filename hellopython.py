6ee9a9be87fffca4317d5e0e5759fc14f9a8003e
mv-mac <misha6213501@yandex.ru>
2018-06-03 18:36:50 +0300

    таблица умножения

--- hellopython.py
+++ hellopython.py
@@ -1,4 +1,14 @@
-for i in range(6):
-    for i in range(6):
-        print('*', end=' ')
+a, b, c, d = int(input()),int(input()),int(input()),int(input())
+
+#строка 1
+print('\t', end='')
+for i in range(c, d+1):
+    print(i,  end= '\t')
+print()
+
+#остальные строки
+for i in range(a, b+1):
+    print(i, end='\t')
+    for j in range(c, d+1):
+        print(i*j, end='\t')
     print()
