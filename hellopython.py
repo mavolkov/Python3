e17c8c2b3de066643c666f63602008c92092d6ee
mv-win <misha6213501@mail.ru>
2018-06-05 16:08:46 +0300

    сумма соседних элементов списка

--- hellopython.py
+++ hellopython.py
@@ -1,4 +1,13 @@
 list = [int(i) for i in input().split()]
-print(sum(list))
+
+if len(list) != 1:
+    for i in range(len(list)-1):
+        print(list[i-1] + list[i+1], end=' ')
+    print(list[len(list)-2]+list[0])
+else:
+    print(list[0])
+
+# 1 3 5 6 10
+
 
 
