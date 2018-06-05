ef85de3ad5ec1b60786c58cc434daf64e77b101f
mv-win <misha6213501@mail.ru>
2018-06-05 15:04:40 +0300

    сумма элементов списка

--- hellopython.py
+++ hellopython.py
@@ -1,13 +1,5 @@
 s = input()
-result = ''
-
-n=1
-for i in range(1,len(s)):
-        if s[i] == s[i-1]:
-                n += 1
-        else:
-                result += s[i-1] + str(n)
-                n = 1
-result += s[len(s)-1] + str(n)
-n = 1
-print(result)
+list = s.split()
+for i in range(len(list)):
+        list[i] = int(list[i])
+print(sum(list))
