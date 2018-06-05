d2df656510bdaf7bbc8c58c0e1c2367686f51881
mv-win <misha6213501@mail.ru>
2018-06-05 18:19:46 +0300

    повторяющиеся в списке

--- hellopython.py
+++ hellopython.py
@@ -1,13 +1,8 @@
 list = [int(i) for i in input().split()]
-
-if len(list) != 1:
-    for i in range(len(list)-1):
-        print(list[i-1] + list[i+1], end=' ')
-    print(list[len(list)-2]+list[0])
-else:
-    print(list[0])
-
-# 1 3 5 6 10
-
-
-
+result = []
+for i in list:
+    if list.count(i) > 1 and i not in result:
+        result.append(i)
+result.sort()
+for i in result:
+    print(i, end=' ')
