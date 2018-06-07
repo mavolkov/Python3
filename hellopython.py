0e080922c39915d00334e70278fa5507ac624202
mv-mac <misha6213501@yandex.ru>
2018-06-07 18:35:09 +0300

    изменение списка c проходом с конца(глобальная переменная) в функции

--- hellopython.py
+++ hellopython.py
@@ -1,18 +1,18 @@
 lst = [int(i) for i in input().split()]
 
+# изменение списка c проходом с конца(глобальная переменная) в функции
+
 
 def modify_list(l1):
-    for i in range(len(l1)):
+    for i in range(len(l1)-1, -1, -1):
         if l1[i] % 2 == 0:
             l1[i] /= 2
             l1[i] = int(l1[i])
         else:
-            l1[i] = -1
-    while -1 in l1:
-        l1.remove(-1)
+            l1.pop(i)
+
 
 
 modify_list(lst)
 print(lst)
 
-#изменение списка(глобальная переменная) в функции
