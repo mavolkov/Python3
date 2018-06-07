ad38bcd16199ebfdb0f95f4bb4b500c75e9478f2
mv-mac <misha6213501@yandex.ru>
2018-06-07 18:18:32 +0300

    изменение списка(глобальная переменная) в функции

--- hellopython.py
+++ hellopython.py
@@ -1,24 +1,18 @@
-'''
-def modify_list(l):
-    for i in range(len(l)):
-        if l[i] % 2 == 0:
-            l[i] = int(l[i]/2)
+lst = [int(i) for i in input().split()]
+
+
+def modify_list(l1):
+    for i in range(len(l1)):
+        if l1[i] % 2 == 0:
+            l1[i] /= 2
+            l1[i] = int(l1[i])
         else:
-            l[i] = -10
-    while -10 in l:
-        l.remove(-10)
-    return l
-'''
-
-list = [int(i) for i in input().split()]
-
-def modify_list():
-    new_l =[]
-    for i in list:
-        if i % 2 == 0:
-            new_l.append(int(i/2))
-    list=new_l
+            l1[i] = -1
+    while -1 in l1:
+        l1.remove(-1)
 
 
-modify_list()
-print(list)
+modify_list(lst)
+print(lst)
+
+#изменение списка(глобальная переменная) в функции
