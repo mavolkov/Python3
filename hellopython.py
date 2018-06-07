fc84f6c13adad1b164b30a5ed1a570eb799970b2
mv-mac <misha6213501@yandex.ru>
2018-06-08 02:05:55 +0300

    множество

--- hellopython.py
+++ hellopython.py
@@ -1,25 +1,13 @@
-# Принимает число n. Заполняет таблицу размером n×n,
-# заполненную числами от 1 до n**2 по спирали
+s = set()
+for i in range(10000000):
+    s.add(i)
 
+x = 0
+for i in s:
+    print(i, end='\t')
+    x += 1
+    if x % 20 == 0:
+        print()
 
-n = int(input())
-
-dx, dy = 1, 0
-x, y = 0, 0
-
-lst = [[0 for i in range(n)] for j in range(n)]
-
-for i in range(1, n ** 2 + 1):
-    lst[x][y] = i
-    nx, ny = x + dx, y + dy
-    if 0 <= nx < n and 0 <= ny < n and not lst[nx][ny]:
-        x, y = nx, ny
-    else:
-        dx, dy = -dy, dx
-        x, y = x + dx, y + dy
-
-for x in list(zip(*lst)):
-    print(*x)
-
-
- 
+print()
+print(555 in s)
