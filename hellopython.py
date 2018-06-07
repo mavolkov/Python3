2ca99f215c0b3a20c085c06ce1d5d11f1619d80d
mv-mac <misha6213501@yandex.ru>
2018-06-07 22:45:48 +0300

    улитка
    Принимает число n. Заполняет таблицу размером n×n, заполненную числами от 1 до n**2 по спирали

--- hellopython.py
+++ hellopython.py
@@ -1,39 +1,24 @@
-# До ввода 'end' записывает двумерный список построчно.
-# В новый список такого же размера записывает число,
-# равное элементов первой матрицы на позициях
-# (i-1, j), (i+1, j), (i, j-1), (i, j+1)
+# Принимает число n. Заполняет таблицу размером n×n,
+# заполненную числами от 1 до n**2 по спирали
 
-lst = []
-double_list = []
-while True:
-    lst = input()
-    if lst != 'end':
-        double_list.append([int(i) for i in lst.split()])
+
+n = int(input())
+
+dx, dy = 1, 0
+x, y = 0, 0
+
+lst = [[0 for i in range(n)] for j in range(n)]
+
+for i in range(1, n ** 2 + 1):
+    lst[x][y] = i
+    nx, ny = x + dx, y + dy
+    if 0 <= nx < n and 0 <= ny < n and not lst[nx][ny]:
+        x, y = nx, ny
     else:
-        break
+        dx, dy = -dy, dx
+        x, y = x + dx, y + dy
 
-result = [[0 for i in range(len(double_list[0]))] for i in range(len(double_list))]
-
-
-for i in range(len(result)):
-    for j in range(len(result[0])):
-        if -len(result)-1 <= i-1 < len(result) and -len(result)-1 <= i+1 < len(result):
-            result[i][j] += double_list[i-1][j]
-            result[i][j] += double_list[i+1][j]
-        else:
-            result[i][j] += double_list[i - 1][j]
-            result[i][j] += double_list[0][j]
-        if -len(result[0])-1 <= j-1 < len(result[0]) and -len(result[0])-1 <= j+1 < len(result[0]):
-            result[i][j] += double_list[i][j-1]
-            result[i][j] += double_list[i][j+1]
-        else:
-            result[i][j] += double_list[i][j - 1]
-            result[i][j] += double_list[i][0]
-
-
-for i in range(len(result)):
-    for j in range(len(result[0])):
-        print(result[i][j], end=' ')
-    print()
+for x in list(zip(*lst)):
+    print(*x)
 
 
