a27e58ff67388f846b2f7672a567ba2bf5408aec
mv-mac <misha6213501@yandex.ru>
2018-06-08 03:16:07 +0300

    перебор эл-в словаря

--- hellopython.py
+++ hellopython.py
@@ -1,3 +1,17 @@
-d = {1:'один', 2:'два'}
-print(type(d))
-print(d[1])
+d = {1: 'apple', 2: 'orange', 3: 'apple', 4: 'pear', 5:'orange', 6:'banana'}
+
+# проход по ключам
+for i in d:
+    print(i)
+
+# проход по ключам
+for i in d.keys():
+    print(i)
+
+# проход по значениям
+for i in d.values():
+    print(i)
+
+# проход по ключ-значение
+for key,value in d.items():
+    print(key, '-', value)
