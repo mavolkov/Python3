6787226403a726d51c406c569f264fc346a38f84
mv-mac <misha6213501@yandex.ru>
2018-06-08 23:21:04 +0300

    кол-во слов в строке через словарь

--- hellopython.py
+++ hellopython.py
@@ -1,18 +1,11 @@
-def update_dictionary(d, key, value):
-    if key in d.keys():
-        d[key].append(value)
-    else:
-        if 2*key in d.keys():
-            d[2*key].append(value)
-        else:
-            d[2*key] = [value]
-
 d = {}
-print(update_dictionary(d, 1, -1))
-print(d)
-update_dictionary(d, 2, -2)
-print(d)
-update_dictionary(d, 1, -3)
-print(d)
+l = [i.lower() for i in input().split()]
 
+for i in l:
+    if i not in d.keys():
+        d[i] = 1
+    else:
+        d[i] +=1
 
+for key, value in d.items():
+    print(key, value)
