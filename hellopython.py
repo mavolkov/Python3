5a5ddb5c2342f938ab5d9e508653b76bdb4c9715
mv-mac <misha6213501@yandex.ru>
2018-06-09 23:51:24 +0300

    1

--- hellopython.py
+++ hellopython.py
@@ -1,11 +1,13 @@
-# f(x) определена ранее
-d = {}
-n = int(input())
-for i in range(n):
-    x = int(input())
-    if x in d.keys():
-        print(d[x])
-    else:
-        print(f(x))
-        d[x] = f(x)
+s = input()
+result = ''
 
+n=1
+for i in range(1,len(s)):
+        if s[i] == s[i-1]:
+                n += 1
+        else:
+                result += s[i-1] + str(n)
+                n = 1
+result += s[len(s)-1] + str(n)
+n = 1
+print(result)
