7d7ec543a9c2f68f940889a89aea5f406ad3857c
mv-mac <misha6213501@yandex.ru>
2018-06-10 04:59:49 +0300

    Оценки студентов
    Записывает в файл среднюю оценку  по 3 предметам каждого абитуриента. На последний строке пишет среднюю оценку по все абитуриентам.

--- hellopython.py
+++ hellopython.py
@@ -1,25 +1,19 @@
 f = open('set.txt', 'r')
 f_out = open('out.txt', 'w')
-a = 'ёёёёё'
-d = {}
+
+s = ['x', 0, 0, 0]
+k = 0
+
 for line in f:
-    l = [i.lower() for i in line.split()]
-    for j in l:
-        if j.isalpha():
-            if j not in d.keys():
-                d[j.strip()] = 1
-            else:
-                d[j] += 1
-
-print(d)
-
-for key, value in d.items():
-    if value == max(d.values()):
-        if key < a:
-            a = key
-#print('самое частое слово: ', a, max(d.values()))
-#print(d)
-f_out.write(a + ' ' + str(max(d.values())))
+    k += 1
+    sum = int(line.split(';')[1]) + int(line.split(';')[2]) + int(line.split(';')[3])
+    # print(sum/3)
+    f_out.write(str(sum/3) + '\n')
+    s[1] += int(line.split(';')[1])
+    s[2] += int(line.split(';')[2])
+    s[3] += int(line.split(';')[3])
+# print(s[1]/k, s[2]/k, s[3]/k)
+f_out.write(str(s[1]/k)+' ' + str(s[2]/k)+' ' + str(s[3]/k)+' ')
 f.close()
 f_out.close()
 
