d21213e3c7a927d9e0417f2482cc248d3a71195e
mv-mac <misha6213501@yandex.ru>
2018-06-12 02:31:58 +0300

    Средний рост школьников по классам
    Средний рост школьников по классам, нет ученика в списко - прочерк

--- hellopython.py
+++ hellopython.py
@@ -1,30 +1,20 @@
-'''
-s = set()
 l = []
-with open('set.txt', 'r') as f:
-    n = int(f.readline())
-    for i in range(n):
-        s.add((f.readline().strip()).lower())
-    n = int(f.readline())
-    for i in range(n):
-        l.append((f.readline().strip()).lower())
-print(s)
-print(l)
-'''
+f = open('set.tsv', 'r')
+for line in f:
+    l.append(line.split())
 
-s = set()
-l = []
-n = int(input())
-for i in range(n):
-    s.add((input().strip()).lower())
-n = int(input())
-for i in range(n):
-    l.append((input().strip()).lower())
 
-user_s = set()
+d = {}
+for i in range(1, 11+1):
+    d[i] = [0.0, 0]
+
 for i in l:
-    for j in i.split():
-        if j not in s:
-            user_s.add(j)
-for i in user_s:
-    print(i)
+    d[int(i[0])][0] += float(i[2])
+    d[int(i[0])][1] += 1
+
+
+for key, value in d.items():
+    if value[1] != 0:
+        print(key, value[0]/value[1])
+    else:
+        print(key, '-')
