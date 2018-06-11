77d93e8a33638ebe46147fcd5139885bc50f39c0
mv-mac <misha6213501@yandex.ru>
2018-06-12 01:14:52 +0300

    кодер и декодер строки по заданному словарю

--- hellopython.py
+++ hellopython.py
@@ -1,53 +1,33 @@
-# читает игры из файла
-#games = []
-# with open('set.txt', 'r') as f:
-#    n = int(f.readline())
-#    for i in range(n):
-#        games.append((f.readline().strip()).split(';'))
-# for i in games:
-#     print(i)
+"""
+s = []
+with open('set.txt', 'r') as f:
+    for i in range(4):
+        s.append(f.readline().strip())
+print(s)
+"""
+
+s = []
+for i in range(4):
+        s.append(input().strip())
+
+coder = {}
+for i in range(len(s[0])):
+    coder[s[0][i]] = s[1][i]
+# for key, value in coder.items():
+#     print(key, '-', value)
 # print()
 
-# читает игры на ввод
-games = []
-n = int(input())
-for i in range(n):
-    games.append((input().strip().split(';')))
+decoder = {}
+for i in range(len(s[0])):
+    decoder[s[1][i]] = s[0][i]
+# for key, value in decoder.items():
+#     print(key, '-', value)
+# print()
 
-# отбираем команды
-teams = {}
-for i in range(n):
-    if games[i][0] not in teams:
-        teams[games[i][0]] = [0, 0, 0, 0, 0]
-    if games[i][2] not in teams:
-        teams[games[i][2]] = [0, 0, 0, 0, 0]
-
-for i in range(n):
-    if games[i][1] == games[i][3]:   # ничья
-        # +1 игра
-        teams[games[i][0]][0] += 1
-        teams[games[i][2]][0] += 1
-        # +1 ничья
-        teams[games[i][0]][2] += 1
-        teams[games[i][2]][2] += 1
-        # +1 балл
-        teams[games[i][0]][4] += 1
-        teams[games[i][2]][4] += 1
-    elif games[i][1] > games[i][3]:  # первая выиграла
-        teams[games[i][0]][0] += 1
-        teams[games[i][2]][0] += 1
-        teams[games[i][0]][1] += 1
-        teams[games[i][2]][3] += 1
-        teams[games[i][0]][4] += 3
-        teams[games[i][2]][4] += 0
-    else:                            # вотрая выиграла
-        teams[games[i][0]][0] += 1
-        teams[games[i][2]][0] += 1
-        teams[games[i][0]][3] += 1
-        teams[games[i][2]][1] += 1
-        teams[games[i][0]][4] += 0
-        teams[games[i][2]][4] += 3
-
-for key, value in teams.items():
-    print(key + ':' + str(value[0]), value[1], value[2], value[3], value[4])
+# coding
+for i in s[2]:
+    print(coder[i], end='')
 print()
+# decoding
+for i in s[3]:
+    print(decoder[i], end='')
