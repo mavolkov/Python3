82857b5ce2f4d4e7756534cd6fdb469c6f49d876
mv-mac <misha6213501@yandex.ru>
2018-06-13 01:17:37 +0300

    .

--- hellopython.py
+++ hellopython.py
@@ -1,77 +1,20 @@
-from pylab import *
-from matplotlib import *
+# идентификатор объекта у каждого свой
+# Получение идентификаторов (отличаются)
+x = [1, 2, 3]
+print(id(x))
+print(id([1, 2, 3]))
 
-x = linspace(0, 5, 10)
-y = x**2
+# оператор is сравнивает идентификаторы
+x = [1, 2, 3]
+y = x
+print(x is y)
+print(y is x)
+print(y is [1, 2, 3])
+
+# изменяется объект, а не переменная
+x = [1, 2, 3]
+y = x
+print(y is x)
+x.append(4)
 print(x)
 print(y)
-'''
-# способ построение графика 1
-# %matplotlib inline #не сработало(должно построить прям в консоле?)
-figure()
-plot(x, y, 'black')
-xlabel('x')
-ylabel('y')
-title('title')
-show()
-'''
-
-'''
-# способ построения графика 2
-fig = plt.figure()
-axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
-
-axes.plot(x, y, 'r')
-axes.set_xlabel('x')
-axes.set_ylabel('y')
-axes.set_title('title1')  # чтобы не отображалась строка <matplotlib.text.Text at ...> в конце можно
-show()
-'''
-
-'''
-# строим 2 графика сразу
-fig = plt.figure()
-
-axes1 = fig.add_axes([0.1, 0.1, 0.8, 0.8])  # main axes
-axes2 = fig.add_axes([0.2, 0.5, 0.4, 0.3])  # inset доп axes
-
-# main figure
-axes1.plot(x, y, 'r')
-axes1.set_xlabel('x')
-axes1.set_ylabel('y')
-axes1.set_title('title')
-
-# insert
-axes2.plot(y, x, 'g')
-axes2.set_xlabel('x')
-axes2.set_ylabel('y')
-axes2.set_title('insert title');
-
-show()  # показываем все
-'''
-
-'''
-# строим 2 графика отдельно 
-fig, axes = plt.subplot(nrows=1, ncols=2)  # не заработало...
-for ax in axes:
-    ax.plot(x, y, 'r')
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_title('title')
-fig.tight_layout()
-'''
-
-'''
-# с регулировкой размера 
-fig = plt.subplot(figsize=(12, 3))  # не заработало..
-axes = plt.subplot(figsize=(12, 3))
-axes.plot(x, y, 'r')
-axes.set_xlabel('x')
-axes.set_ylabel('y')
-axes.set_title('title')
-# show()
-'''
-
-
-
-
