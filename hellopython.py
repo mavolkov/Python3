97a9905bd35dd002906d12d99bc388c1e74fa416
mv-mac <misha6213501@yandex.ru>
2018-06-26 21:46:49 +0300

    исключения

--- hellopython.py
+++ hellopython.py
@@ -1,20 +1,11 @@
-# идентификатор объекта у каждого свой
-# Получение идентификаторов (отличаются)
-x = [1, 2, 3]
-print(id(x))
-print(id([1, 2, 3]))
+x = input()
+y = input()
 
-# оператор is сравнивает идентификаторы
-x = [1, 2, 3]
-y = x
-print(x is y)
-print(y is x)
-print(y is [1, 2, 3])
-
-# изменяется объект, а не переменная
-x = [1, 2, 3]
-y = x
-print(y is x)
-x.append(4)
-print(x)
-print(y)
+try:
+    result = int(x)/int(y)
+except (ZeroDivisionError, ValueError, TypeError):
+    print('ошибка =(')
+else:
+    print('result is', result)
+finally:
+    print('end')
